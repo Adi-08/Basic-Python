c3d8b41c558362698d6af49c6bbd82eadb425c2e
Adi-08 <adityassalunke@gmail.com>
2026-06-30 13:56:28 +0530

    2nd program

--- List/06_remove_elements.py
+++
@@ -1,9 +0,0 @@
-marks = [10,20,30.2,35.6,55,99]
-
-# .remove() remove specific element
-marks.remove(30.2)
-print(marks)
-
-# .pop() by default removes last element
-marks.pop()
-print(marks)
--- List/07_count_occurrences.py
+++
@@ -1,5 +0,0 @@
-marks = [10,20,30.2,35.6,55,99,10]
-
-# use .count() method to find occurrence  
-count = marks.count(10)
-print(count)
