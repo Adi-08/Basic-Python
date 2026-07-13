d8cb89c9308b0b2417e58eef49d3b905bd2592a0
Adi-08 <adityassalunke@gmail.com>
2026-07-13 22:28:22 +0530

    forLoop

---
+++ ForLoop/evenOdd.py
@@ -0,0 +1,18 @@
+# Even
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
+
+# using steps
+for i in range(2,11,2):
+    print(i)
+
+print("---------------------")
+# Odd
+for i in range(1,11):
+    if i% 2 !=0:
+        print(i)
+print("----=========-----")
+# using step
+for i in range(1,11,2):
+    print(i)
